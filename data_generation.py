1022f8537b20eed53d077f89c35e261da6fc73e1
Arnab Sarker <arnabs@seas.upenn.edu>
2018-12-07 13:12:38 -0500

    Better modularity, added tests from 620 Project

---
+++ data_generation.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+import scipy
+
+## Create one linear classifier, and k-1 "barriers" such that,
+## if point x lies between barrier i and i-1, the probability it belongs to class i
+## is proportional to how close it is to that barrier
+def generate_quantile_data(d, k, n, portions):
+    ## Vector that classification will be based on
+    w = np.array([1/d]*d)
+    scaling = portions[-1]
+
+    ## Generate points
+    projection_points = np.matrix(np.random.uniform(0, scaling, n)).T
+    points_on_line = projection_points * w
+
+    #Ensure noise in range as to not mess with classification too much
+    noise = np.reshape(np.matrix(np.random.normal(0, scaling / (16*d), n*d)), (n, d))
+    noise = noise - np.dot(noise, w).T * w
+    X = points_on_line + noise
+
+    ## Get class labels
+    y = get_class_labels(projection_points, portions, k)
+    
+    return X,y
+
+def get_class_labels(projection_points, portions, k):
+    y = np.matrix(np.zeros(projection_points.size)).T
+    for i in range(0, projection_points.size):
+        curr_point = projection_points[i]
+        likely_class = np.argmax((curr_point - portions) < 0)
+        if(likely_class == k - 1):
+            y[i] = k-1
+        else:
+            if(likely_class > 0):
+                prob_up = 1 - (portions[likely_class] - curr_point) / (portions[likely_class] - portions[likely_class - 1])
+            else:
+                prob_up = 1 - (portions[likely_class] - curr_point) / (portions[likely_class] - 0)
+            y[i] = likely_class + 1 * (np.random.uniform(0,1) < prob_up)
+    return y
+
+def generate_linear_complex_data(d, k, n, portions):
+    ## Vector that classification will be based on
+    w = np.array([1/d]*d)
+    scaling = portions[-1]
+
+    ## Generate points
+    projection_points = np.matrix(np.random.uniform(0, scaling, n)).T
+    points_on_line = projection_points * w
+
+    #Ensure noise in range as to not mess with classification too much
+    noise = np.reshape(np.matrix(np.random.normal(0, scaling / (16*d), n*d)), (n, d))
+    noise = noise - np.dot(noise, w).T * w
+    X = points_on_line + noise
+
+    ## Get class labels
+    y = get_many_class_labels(projection_points, portions, k)
+    
+    return X,y
+
+def get_many_class_labels(projection_points, portions, k):
+    y = np.matrix(np.zeros(projection_points.size)).T
+    for i in range(0, projection_points.size):
+        curr_point = projection_points[i]
+        probs = 1 / np.power(np.abs(portions - curr_point), 2)
+        probs = np.asarray(probs / np.sum(probs))
+        y[i] = np.random.choice(k, p=probs[0])
+    return y
+
+## Create one linear classifier, and k-1 "barriers" such that,
+## if point x lies between barrier i and i-1, the probability it belongs to class i
+## is proportional to how close it is to that barrier
+def generate_quantile_testdata(d, k, n, portions):
+    ## Vector that classification will be based on
+    w = np.array([1/d]*d)
+    scaling = portions[-1]
+
+    ## Generate points
+    projection_points = np.matrix(np.random.uniform(0, scaling, n)).T
+    print(projection_points)
+    points_on_line = projection_points * w
+    print(points_on_line)
+
+    #Ensure noise in range as to not mess with classification too much
+    noise = np.reshape(np.matrix(np.random.normal(0, scaling / (16*d), n*d)), (n, d))
+    noise = noise - np.dot(noise, w).T * w
+    X = points_on_line + noise
+    print(X.dot(w) *d)
+    
+    return X
+
+def generate_quantile_complex_testdata(d, k, n, alpha, portions):
+    ## Vector that classification will be based on
+    w = np.array([1/d]*d)
+    scaling = portions[-1]
+
+    ## Generate points
+    projection_points = np.matrix(np.random.uniform(0, scaling, n)).T
+    points_on_line = projection_points * w
+
+    #Ensure noise in range as to not mess with classification too much
+    noise = np.reshape(np.matrix(np.random.normal(0, scaling / (16*d), n*d)), (n, d))
+    noise = noise - np.dot(noise, w).T * w
+    X = points_on_line + noise
+    
+    y = get_many_class_quantiles(projection_points, portions, k, alpha)
+    
+    return X, y
+
+def get_many_class_quantiles(projection_points, portions, k, alpha):
+    y = np.matrix(np.zeros(projection_points.size)).T
+    for i in range(0, projection_points.size):
+        curr_point = projection_points[i]
+        probs = 1 / np.power(np.abs(portions - curr_point), 2)
+        probs = np.asarray(probs / np.sum(probs))
+        y[i] = np.argmax(np.cumsum(probs) > alpha)
+    return y
