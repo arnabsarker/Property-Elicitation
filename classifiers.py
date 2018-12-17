d849a19feda4fe4e8c84bd745f0b8ca85f7be726
Arnab Sarker <arnabs@seas.upenn.edu>
2018-12-16 21:42:48 -0500

    Added hinge loss, only works for AT linear

--- classifiers.py
+++ classifiers.py
@@ -4,165 +4,6 @@ import scipy
 from sklearn import base, metrics
 from sklearn.utils.validation import check_X_y
 
-class LogisticAT(base.BaseEstimator):
-    """
-    Classifier that implements the ordinal logistic model (All-Threshold variant)
-
-    Parameters
-    ----------
-    alpha: float
-        Regularization parameter. Zero is no regularization, higher values
-        increate the squared l2 regularization.
-
-    References
-    ----------
-    J. D. M. Rennie and N. Srebro, "Loss Functions for Preference Levels :
-    Regression with Discrete Ordered Labels," in Proceedings of the IJCAI
-    Multidisciplinary Workshop on Advances in Preference Handling, 2005.
-    """
-    def __init__(self, alpha=1., verbose=0, max_iter=1000):
-        self.alpha = alpha
-        self.verbose = verbose
-        self.max_iter = max_iter
-
-    def fit(self, X, y, sample_weight=None):
-        _y = np.array(y).astype(np.int)
-        if np.abs(_y - y).sum() > 0.1:
-            raise ValueError('y must only contain integer values')
-        self.classes_ = np.unique(y)
-        self.n_class_ = self.classes_.max() - self.classes_.min() + 1
-        y_tmp = y - y.min()  # we need classes that start at zero
-        self.coef_, self.theta_ = threshold_fit(
-            X, y_tmp, self.alpha, self.n_class_, mode='AE',
-            verbose=self.verbose, max_iter=self.max_iter,
-            sample_weight=sample_weight)
-        return self
-
-    def predict(self, X):
-        return threshold_predict(X, self.coef_, self.theta_) +\
-         self.classes_.min()
-
-    def predict_proba(self, X):
-        return threshold_proba(X, self.coef_, self.theta_)
-
-    def score(self, X, y, sample_weight=None):
-        pred = self.predict(X)
-        return -metrics.mean_absolute_error(
-            pred,
-            y,
-            sample_weight=sample_weight)
-
-
-class LogisticIT(base.BaseEstimator):
-    """
-    Classifier that implements the ordinal logistic model
-    (Immediate-Threshold variant).
-
-    Contrary to the OrdinalLogistic model, this variant
-    minimizes a convex surrogate of the 0-1 loss, hence
-    the score associated with this object is the accuracy
-    score, i.e. the same score used in multiclass
-    classification methods (sklearn.metrics.accuracy_score).
-
-    Parameters
-    ----------
-    alpha: float
-        Regularization parameter. Zero is no regularization, higher values
-        increate the squared l2 regularization.
-
-    References
-    ----------
-    J. D. M. Rennie and N. Srebro, "Loss Functions for Preference Levels :
-    Regression with Discrete Ordered Labels," in Proceedings of the IJCAI
-    Multidisciplinary Workshop on Advances in Preference Handling, 2005.
-    """
-    def __init__(self, alpha=1., verbose=0, max_iter=1000):
-        self.alpha = alpha
-        self.verbose = verbose
-        self.max_iter = max_iter
-
-    def fit(self, X, y, sample_weight=None):
-        _y = np.array(y).astype(np.int)
-        if np.abs(_y - y).sum() > 0.1:
-            raise ValueError('y must only contain integer values')
-        self.classes_ = np.unique(y)
-        self.n_class_ = self.classes_.max() - self.classes_.min() + 1
-        y_tmp = y - y.min()  # we need classes that start at zero
-        self.coef_, self.theta_ = threshold_fit(
-            X, y_tmp, self.alpha, self.n_class_,
-            mode='0-1', verbose=self.verbose, max_iter=self.max_iter,
-            sample_weight=sample_weight)
-        return self
-
-    def predict(self, X):
-        return threshold_predict(X, self.coef_, self.theta_) +\
-         self.classes_.min()
-
-    def predict_proba(self, X):
-        return threshold_proba(X, self.coef_, self.theta_)
-
-    def score(self, X, y, sample_weight=None):
-        pred = self.predict(X)
-        return metrics.accuracy_score(
-            pred,
-            y,
-            sample_weight=sample_weight)
-
-
-class LogisticSE(base.BaseEstimator):
-    """
-    Classifier that implements the ordinal logistic model
-    (Squared Error variant).
-
-    Contrary to the OrdinalLogistic model, this variant
-    minimizes a convex surrogate of the 0-1 (?) loss ...
-
-    TODO: double check this description (XXX)
-
-    Parameters
-    ----------
-    alpha: float
-        Regularization parameter. Zero is no regularization, higher values
-        increase the squared l2 regularization.
-
-    References
-    ----------
-    J. D. M. Rennie and N. Srebro, "Loss Functions for Preference Levels :
-    Regression with Discrete Ordered Labels," in Proceedings of the IJCAI
-    Multidisciplinary Workshop on Advances in Preference Handling, 2005.
-    """
-    def __init__(self, alpha=1., verbose=0, max_iter=100000):
-        self.alpha = alpha
-        self.verbose = verbose
-        self.max_iter = max_iter
-
-    def fit(self, X, y, sample_weight=None):
-        _y = np.array(y).astype(np.int)
-        if np.abs(_y - y).sum() > 1e-3:
-            raise ValueError('y must only contain integer values')
-        self.classes_ = np.unique(y)
-        self.n_class_ = self.classes_.max() - self.classes_.min() + 1
-        y_tmp = y - y.min()  # we need classes that start at zero
-        self.coef_, self.theta_ = threshold_fit(
-            X, y_tmp, self.alpha, self.n_class_,
-            mode='SE', verbose=self.verbose, max_iter=self.max_iter,
-            sample_weight=sample_weight)
-        return self
-
-    def predict(self, X):
-        return threshold_predict(X, self.coef_, self.theta_) +\
-         self.classes_.min()
-
-    def predict_proba(self, X):
-        return threshold_proba(X, self.coef_, self.theta_)
-
-    def score(self, X, y, sample_weight=None):
-        pred = self.predict(X)
-        return -metrics.mean_squared_error(
-            pred,
-            y,
-            sample_weight=sample_weight)
-
 """
 This implements the margin-based ordinal regression methods described
 in http://arxiv.org/abs/1408.2327
@@ -178,6 +19,10 @@ def sigmoid(t):
     out[~idx] = exp_t / (1. + exp_t)
     return out
 
+def hinge_derive(t):
+    out = -t * (t < 1)
+    return out
+
 
 def log_loss(Z):
     # stable computation of the logistic loss
@@ -187,16 +32,19 @@ def log_loss(Z):
     out[~idx] = (-Z[~idx] + np.log(1 + np.exp(Z[~idx])))
     return out
 
-def direct_loss(Z):
-    # Weighted surrogate 
-    out = np.max(1 - Z, 0)
+def hinge_loss(Z):
+    idx = Z > 0
+    out = np.zeros_like(Z)
+    out[idx] = np.maximum(1 - Z[idx], 0)
+    out[~idx] = np.maximum(1 - Z[~idx], 0)
     return out
 
-def obj_margin(x0, X, y, alpha, n_class, weights, L, kernel_type, sample_weight):
+def obj_margin(x0, X, y, alpha, n_class, weights, L, kernel_type, loss_function, sample_weight):
     """
     Objective function for the general margin-based formulation
     """
 
+    print(x0)
     w = x0[:X.shape[1]]
     c = x0[X.shape[1]:]
     theta = L.dot(c)
@@ -206,7 +54,11 @@ def obj_margin(x0, X, y, alpha, n_class, weights, L, kernel_type, sample_weight)
     Alpha = theta[:, None] - Xw  # (n_class - 1, n_samples)
     S = np.sign(np.arange(n_class - 1)[:, None] - y + 0.5)
 
-    err = loss_fd.T * log_loss(S * Alpha)
+    if(loss_function == 'logistic'):
+        err = loss_fd.T * log_loss(S * Alpha)
+    elif(loss_function == 'hinge'):
+        err = loss_fd.T * hinge_loss(S * Alpha)
+        
     if sample_weight is not None:
         err *= sample_weight
     obj = np.sum(err)
@@ -216,6 +68,7 @@ def obj_margin(x0, X, y, alpha, n_class, weights, L, kernel_type, sample_weight)
         obj += alpha * 0.5 * (np.dot(w, w))
     else:
         obj += alpha * 0.5 * np.dot(np.matmul(w.T, X).T, w)
+    
     return obj
 
 def obj_direct(x0, X, y, alpha, gamma, n_class, weights, L, kernel_type, sample_weight):
@@ -266,7 +119,7 @@ def grad_direct(x0, X, y, alpha, gamma, n_class, weights, L, kernel_type, sample
     grad_c = L.T.dot(grad_theta)
     return np.concatenate((grad_w, grad_c), axis=0)
 
-def grad_margin(x0, X, y, alpha, n_class, weights, L, kernel_type, sample_weight):
+def grad_margin(x0, X, y, alpha, n_class, weights, L, kernel_type, loss_function, sample_weight):
     """
     Gradient for the general margin-based formulation
     """
@@ -279,10 +132,11 @@ def grad_margin(x0, X, y, alpha, n_class, weights, L, kernel_type, sample_weight
     Xw = X.dot(w)
     Alpha = theta[:, None] - Xw  # (n_class - 1, n_samples)
     S = np.sign(np.arange(n_class - 1)[:, None] - y + 0.5)
-    # Alpha[idx] *= -1
-    # W[idx.T] *= -1
 
-    Sigma = S * loss_fd.T * sigmoid(-S * Alpha)
+    if(loss_function == 'logistic'):
+        Sigma = S * loss_fd.T * sigmoid(-S * Alpha)
+    elif(loss_function == 'hinge'):
+        Sigma = S * loss_fd.T * hinge_derive(-S * Alpha)
     if sample_weight is not None:
         Sigma *= sample_weight
 
@@ -296,68 +150,7 @@ def grad_margin(x0, X, y, alpha, n_class, weights, L, kernel_type, sample_weight
     return np.concatenate((grad_w, grad_c), axis=0)
 
 
-def threshold_fit(X, y, alpha, n_class, mode='AE',
-                  max_iter=1000, verbose=False, tol=1e-12,
-                  sample_weight=None):
-    """
-    Solve the general threshold-based ordinal regression model
-    using the logistic loss as surrogate of the 0-1 loss
-
-    Parameters
-    ----------
-    mode : string, one of {'AE', '0-1', 'SE'}
-
-    """
-
-    X, y = check_X_y(X, y, accept_sparse='csr')
-    unique_y = np.sort(np.unique(y))
-    if not np.all(unique_y == np.arange(unique_y.size)):
-        raise ValueError(
-            'Values in y must be %s, instead got %s'
-            % (np.arange(unique_y.size), unique_y))
-
-    n_samples, n_features = X.shape
-
-    # convert from c to theta
-    L = np.zeros((n_class - 1, n_class - 1))
-    L[np.tril_indices(n_class-1)] = 1.
-
-    if mode == 'AE':
-        # loss forward difference
-        loss_fd = np.ones((n_class, n_class - 1))
-    elif mode == '0-1':
-        loss_fd = np.diag(np.ones(n_class - 1)) + \
-            np.diag(np.ones(n_class - 2), k=-1)
-        loss_fd = np.vstack((loss_fd, np.zeros(n_class - 1)))
-        loss_fd[-1, -1] = 1  # border case
-    elif mode == 'SE':
-        a = np.arange(n_class-1)
-        b = np.arange(n_class)
-        loss_fd = np.abs((a - b[:, None])**2 - (a - b[:, None]+1)**2)
-    else:
-        raise NotImplementedError
-
-    x0 = np.zeros(n_features + n_class - 1)
-    x0[X.shape[1]:] = np.arange(n_class - 1)
-    options = {'maxiter' : max_iter, 'disp': verbose}
-    if n_class > 2:
-        bounds = [(None, None)] * (n_features + 1) + \
-                 [(0, None)] * (n_class - 2)
-    else:
-        bounds = None
-
-    sol = optimize.minimize(obj_margin, x0, method='L-BFGS-B',
-        jac=grad_margin, bounds=bounds, options=options,
-        args=(X, y, alpha, n_class, loss_fd, L, sample_weight),
-        tol=tol)
-    if verbose and not sol.success:
-        print(sol.message)
-
-    w, c = sol.x[:X.shape[1]], sol.x[X.shape[1]:]
-    theta = L.dot(c)
-    return w, theta
-
-def threshold_fit_quantile(X, y, alpha, gamma, n_class, kernel_type, 
+def threshold_fit_quantile(X, y, alpha, gamma, n_class, kernel_type, loss_function,
                            mode='QE', max_iter=1000, verbose=False, tol=1e-12,
                            sample_weight=None):
     """
@@ -391,7 +184,7 @@ def threshold_fit_quantile(X, y, alpha, gamma, n_class, kernel_type,
     elif mode == 'AE':
         # loss forward difference
         loss_fd = np.ones((n_class, n_class - 1)) * (1 - gamma)
-        lower_indices = np.tril_indices(n = n_class, m = (n_class - 1), k = -1)
+        lower_indices = np.tril_indices(n = n_class, k = -1)
         loss_fd[lower_indices] = gamma
     else:
         raise NotImplementedError
@@ -407,13 +200,13 @@ def threshold_fit_quantile(X, y, alpha, gamma, n_class, kernel_type,
 
     if (mode != 'Direct'): 
         sol = optimize.minimize(obj_margin, x0, method='L-BFGS-B',
-            jac=grad_margin, bounds=bounds, options=options,
-            args=(X, y, alpha, n_class, loss_fd, L, kernel_type, sample_weight),
+            bounds=bounds, options=options,
+            args=(X, y, alpha, n_class, loss_fd, L, kernel_type, loss_function, sample_weight),
             tol=tol)
     else:
         sol = optimize.minimize(obj_direct, x0, method='L-BFGS-B',
             jac=grad_direct, bounds=bounds, options=options,
-            args=(X, y, alpha, gamma, n_class, loss_fd, L, kernel_type, sample_weight),
+            args=(X, y, alpha, gamma, n_class, loss_fd, L, kernel_type, loss_function, sample_weight),
             tol=tol)
     
     if verbose and not sol.success:
@@ -460,13 +253,15 @@ class LogisticQuantileIT(base.BaseEstimator):
     Classifier that implements the ordinal logistic model for quantile estimation
 
     """
-    def __init__(self, gamma=0.5, alpha=1., verbose=0, max_iter=1000, kernel_type='linear', kernel_param=1):
+    def __init__(self, gamma=0.5, alpha=1., verbose=0, max_iter=1000, 
+                 kernel_type='linear', kernel_param=1, loss_function='logistic'):
         self.gamma = gamma
         self.alpha = alpha
         self.verbose = verbose
         self.max_iter = max_iter
         self.kernel_type = kernel_type
         self.kernel_param = kernel_param
+        self.loss_function = loss_function
 
     def fit(self, X, y, sample_weight=None):
         _y = np.array(y).astype(np.int)
@@ -482,7 +277,7 @@ class LogisticQuantileIT(base.BaseEstimator):
         y_tmp = y - y.min()  # we need classes that start at zero
         self.coef_, self.theta_ = threshold_fit_quantile(
             K, y_tmp, self.alpha, self.gamma, self.n_class_, self.kernel_type,
-            mode='0-1', verbose=self.verbose, max_iter=self.max_iter,
+            self.loss_function, mode='0-1', verbose=self.verbose, max_iter=self.max_iter,
             sample_weight=sample_weight)
         return self
 
@@ -511,13 +306,15 @@ class LogisticQuantileAT(base.BaseEstimator):
     Classifier that implements the ordinal logistic model for quantile estimation
 
     """
-    def __init__(self, gamma=0.5, alpha=1., verbose=0, max_iter=1000, kernel_type='linear', kernel_param=1):
+    def __init__(self, gamma=0.5, alpha=1., verbose=0, max_iter=1000, 
+                 kernel_type='linear', kernel_param=1, loss_function='logistic'):
         self.gamma = gamma
         self.alpha = alpha
         self.verbose = verbose
         self.max_iter = max_iter
         self.kernel_type = kernel_type
         self.kernel_param = kernel_param
+        self.loss_function = loss_function
 
     def fit(self, X, y, sample_weight=None):
         _y = np.array(y).astype(np.int)
@@ -533,7 +330,7 @@ class LogisticQuantileAT(base.BaseEstimator):
         y_tmp = y - y.min()  # we need classes that start at zero
         self.coef_, self.theta_ = threshold_fit_quantile(
             K, y_tmp, self.alpha, self.gamma, self.n_class_, self.kernel_type,
-            mode='AE', verbose=self.verbose, max_iter=self.max_iter,
+            self.loss_function, mode='AE', verbose=self.verbose, max_iter=self.max_iter,
             sample_weight=sample_weight)
         return self
 
