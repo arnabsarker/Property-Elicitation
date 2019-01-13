be28ef49826299725dafa921104aef0808fa256d
Arnab Sarker <arnabs@seas.upenn.edu>
2019-01-13 16:03:21 -0500

    Fixed data storage

--- data_storage.py
+++ data_storage.py
@@ -9,9 +9,22 @@ def init(k):
     global X_test
     global y_test
     
-    X_train, y_train = generate_simplex_data(k,n)
-    X_test, y_test = generate_simplex_data(k,n)
-    classes = np.arange(100)
+    train_path = 'datasets/sector_train.csv'
+    test_path = 'datasets/sector_test.csv'
+    y_col = 0
+    k = 105
+    classes = np.arange(k)
+    
+    
+    ## Import data and get sets
+    log('Retrieving data as matrices', style='w')
+    train_set = np.genfromtxt(train_path, delimiter=',')
+    test_set = np.genfromtxt(test_path, delimiter=',')
 
-    y_train = np.array(y_train).astype(int)
-    y_test = np.array(y_test).astype(int)
+    X_train, y_train = get_features_and_labels(train_set, y_col)
+    X_test, y_test = get_features_and_labels(test_set, y_col)
+
+    #Normalize Data
+    log('Normalizing data')
+    X_train, y_train = (X_train - np.mean(X_train)), y_train
+    X_test, y_test = (X_test - np.mean(X_test)) , y_test
